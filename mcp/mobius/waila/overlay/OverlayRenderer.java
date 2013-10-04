a67d4b0c82df0917037fbcd64374dc493f39b65c
ProfMobius <ProfMobius@localhost>
2013-10-04 14:55:01 +0200

    Added support for entities. Added missing files.

--- mcp/mobius/waila/overlay/OverlayRenderer.java
+++ mcp/mobius/waila/overlay/OverlayRenderer.java
@@ -12,6 +12,9 @@ import org.lwjgl.opengl.GL12;
 import codechicken.nei.api.IHighlightHandler;
 import codechicken.nei.api.ItemInfo;
 import codechicken.nei.forge.GuiContainerManager;
+import cpw.mods.fml.common.ModContainer;
+import cpw.mods.fml.common.registry.EntityRegistry;
+import cpw.mods.fml.common.registry.EntityRegistry.EntityRegistration;
 
 import java.awt.Dimension;
 import java.awt.Point;
@@ -23,6 +26,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.texture.TextureManager;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -37,12 +41,14 @@ public class OverlayRenderer {
     public static void renderOverlay()
     {
         Minecraft mc = Minecraft.getMinecraft();
-        if(mc.currentScreen == null &&
-                mc.theWorld != null &&
-                !mc.gameSettings.keyBindPlayerList.pressed &&
-                ConfigHandler.instance().getConfig(Constants.CFG_WAILA_SHOW, true) &&
-                mc.objectMouseOver != null && 
-                mc.objectMouseOver.typeOfHit == EnumMovingObjectType.TILE)
+        if(!(mc.currentScreen == null &&
+             mc.theWorld != null &&
+             !mc.gameSettings.keyBindPlayerList.pressed &&
+             ConfigHandler.instance().getConfig(Constants.CFG_WAILA_SHOW, true) &&
+             mc.objectMouseOver != null)) 
+        	return;
+    
+        if (mc.objectMouseOver.typeOfHit == EnumMovingObjectType.TILE)
         {
             World world = mc.theWorld;
             ArrayList<ItemStack> items = RayTracing.getIdentifierItems();
@@ -61,9 +67,35 @@ public class OverlayRenderer {
             ItemStack stack = items.get(0);
             renderOverlay(stack, ItemInfo.getText(stack, world, mc.thePlayer, RayTracing.raytracedTarget), getPositioning());
         }
+        
+        if (mc.objectMouseOver.typeOfHit == EnumMovingObjectType.ENTITY)
+        {
+        	Entity ent = mc.objectMouseOver.entityHit;
+        	String modName = getEntityMod(ent);
+        	
+        	List<String> textData = new ArrayList<String>();
+        	try{
+        		textData.add("\u00a7f" + ent.getEntityName());
+        	} catch (Exception e){
+        		textData.add("\u00a7f" + "Unknown");
+        	}
+        	textData.add("\u00a79\u00a7o" + modName);
+        	
+        	renderOverlay(ent, textData, getPositioning());
+        }
     }		
 	
-   
+    private static String getEntityMod(Entity entity){
+    	String modName = "";
+    	try{
+    		EntityRegistration er = EntityRegistry.instance().lookupModSpawn(entity.getClass(), true);
+    		ModContainer modC = er.getContainer();
+    		modName = modC.getName();
+    	} catch (NullPointerException e){
+    		modName = "Minecraft";	
+    	}
+    	return modName;
+    }
 	
     private static Point getPositioning()
     {
@@ -98,5 +130,31 @@ public class OverlayRenderer {
         if (stack.getItem() != null)
             GuiContainerManager.drawItem(x+5, y+h/2-8, stack);
     }    
+    
+    public static void renderOverlay(Entity entity, List<String> textData, Point pos)
+    {
+        int w = 0;
+        for (String s : textData)
+            w = Math.max(w, getStringWidth(s)+10);
+        int h = Math.max(24, 10 + 10*textData.size());
+
+        Dimension size = displaySize();
+        int x = (size.width-w-1)*pos.x/10000;
+        int y = (size.height-h-1)*pos.y/10000;
+        
+        GL11.glDisable(GL12.GL_RESCALE_NORMAL);
+        RenderHelper.disableStandardItemLighting();
+        GL11.glDisable(GL11.GL_LIGHTING);
+        GL11.glDisable(GL11.GL_DEPTH_TEST);
+        
+        drawTooltipBox(x, y, w, h);
+
+        int ty = (h-8*textData.size())/2;
+        for (int i = 0; i < textData.size(); i++)
+            drawString(textData.get(i), x + 6, y + ty + 10*i, 0xFFA0A0A0, true);
+
+        RenderHelper.enableGUIStandardItemLighting();
+        GL11.glEnable(GL12.GL_RESCALE_NORMAL);
+    }     
     
 }
---
+++ mcp/mobius/waila/overlay/RayTracing.java
@@ -0,0 +1,119 @@
+package mcp.mobius.waila.overlay;
+
+import java.util.ArrayList;
+
+import codechicken.nei.api.IHighlightHandler;
+import codechicken.nei.api.ItemInfo;
+import mcp.mobius.waila.Constants;
+import mcp.mobius.waila.addons.ConfigHandler;
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
+import net.minecraft.world.World;
+import net.minecraftforge.common.IShearable;
+
+public class RayTracing {
+
+	public static MovingObjectPosition raytracedTarget = null;
+	
+	public static void raytrace(){
+		if (ConfigHandler.instance().getConfig(Constants.CFG_WAILA_LIQUID))
+			raytracedTarget = getLiquidRaytrace();
+		else
+			raytracedTarget = Minecraft.getMinecraft().objectMouseOver;
+	}
+	
+    public static MovingObjectPosition getLiquidRaytrace()
+    {
+    	EntityPlayer player = Minecraft.getMinecraft().thePlayer;
+    	World world = Minecraft.getMinecraft().theWorld;
+    	
+        float f = 1.0F;
+        float f1 = player.prevRotationPitch + (player.rotationPitch - player.prevRotationPitch) * f;
+        float f2 = player.prevRotationYaw + (player.rotationYaw - player.prevRotationYaw) * f;
+        double d0 = player.prevPosX + (player.posX - player.prevPosX) * (double)f;
+        double d1 = player.prevPosY + (player.posY - player.prevPosY) * (double)f + 1.62D - (double)player.yOffset;
+        double d2 = player.prevPosZ + (player.posZ - player.prevPosZ) * (double)f;
+        Vec3 vec3 = world.getWorldVec3Pool().getVecFromPool(d0, d1, d2);
+        float f3 = MathHelper.cos(-f2 * 0.017453292F - (float)Math.PI);
+        float f4 = MathHelper.sin(-f2 * 0.017453292F - (float)Math.PI);
+        float f5 = -MathHelper.cos(-f1 * 0.017453292F);
+        float f6 = MathHelper.sin(-f1 * 0.017453292F);
+        float f7 = f4 * f5;
+        float f8 = f3 * f5;
+        double d3 = 5.0D;
+        Vec3 vec31 = vec3.addVector((double)f7 * d3, (double)f6 * d3, (double)f8 * d3);
+        return world.clip(vec3, vec31, true);
+        
+    }
+    
+    public static ArrayList<ItemStack> getIdentifierItems()
+    {
+    	raytrace();
+        ArrayList<ItemStack> items = new ArrayList<ItemStack>();
+        
+    	if (raytracedTarget == null)
+    		return items;
+    	
+    	EntityPlayer player = Minecraft.getMinecraft().thePlayer;
+    	World world = Minecraft.getMinecraft().theWorld; 
+    	
+        int x = raytracedTarget.blockX;
+        int y = raytracedTarget.blockY;
+        int z = raytracedTarget.blockZ;
+        Block mouseoverBlock = Block.blocksList[world.getBlockId(x, y, z)];
+        
+        
+        ArrayList<IHighlightHandler> handlers = new ArrayList<IHighlightHandler>();
+        if(ItemInfo.highlightIdentifiers.get(0) != null)
+        	handlers.addAll(ItemInfo.highlightIdentifiers.get(0));
+        if(ItemInfo.highlightIdentifiers.get(mouseoverBlock.blockID) != null)
+        	handlers.addAll(ItemInfo.highlightIdentifiers.get(mouseoverBlock.blockID));
+        
+        for(IHighlightHandler ident : handlers)
+        {
+            ItemStack item = ident.identifyHighlight(world, player, raytracedTarget);
+            if(item != null)
+                items.add(item);
+        }
+        
+        if(items.size() > 0)
+            return items;
+
+        if (world.getBlockTileEntity(x, y, z) == null){
+	        try{
+	        	ItemStack block = new ItemStack(mouseoverBlock, 1, world.getBlockMetadata(x, y, z));
+	        	items.add(block);
+	        } catch(Exception e){}
+        }
+        
+        ItemStack pick = mouseoverBlock.getPickBlock(raytracedTarget, world, x, y, z);
+        if(pick != null)
+            items.add(pick);
+        
+        try
+        {
+            items.addAll(mouseoverBlock.getBlockDropped(world, x, y, z, world.getBlockMetadata(x, y, z), 0));
+        }
+        catch(Exception e){}
+        
+        if(mouseoverBlock instanceof IShearable)
+        {
+            IShearable shearable = (IShearable)mouseoverBlock;
+            if(shearable.isShearable(new ItemStack(Item.shears), world, x, y, z))
+            {
+                items.addAll(shearable.onSheared(new ItemStack(Item.shears), world, x, y, z, 0));
+            }
+        }
+        
+        if(items.size() == 0)
+            items.add(0, new ItemStack(mouseoverBlock, 1, world.getBlockMetadata(x, y, z)));
+        
+        return items;
+    }	    
+}
