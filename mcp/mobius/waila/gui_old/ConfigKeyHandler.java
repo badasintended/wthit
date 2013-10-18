8392a5462bfcda6c386e8cc50eca0eda5f098656
ProfMobius <ProfMobius@localhost>
2013-10-18 11:59:11 +0200

    Fixed derp in calling wrong recipe method.

--- mcp/mobius/waila/gui_old/ConfigKeyHandler.java
+++ mcp/mobius/waila/gui_old/ConfigKeyHandler.java
@@ -117,7 +117,6 @@ public class ConfigKeyHandler extends KeyHandler {
 					firstInventory = false;
 				}					
 		
-				// We check for recipes with metadata, and if we don't find any, we switch to a lookup without metadata
 				if(recipe)
 					if(!GuiCraftingRecipe.openRecipeGui("item", stacks.get(0).copy())){
 						ItemStack target = stacks.get(0).copy();
@@ -132,8 +131,8 @@ public class ConfigKeyHandler extends KeyHandler {
 				if(!recipe)
 					if(!GuiUsageRecipe.openRecipeGui("item", stacks.get(0).copy())){
 						ItemStack target = stacks.get(0).copy();
-						target.setItemDamage(0);
-						if(!GuiCraftingRecipe.openRecipeGui("item", target)){
+						target.setItemDamage(0);						
+						if(!GuiUsageRecipe.openRecipeGui("item", target)){
 							mc.thePlayer.addChatMessage("\u00a7f\u00a7oNo usage found.");
 							mc.displayGuiScreen((GuiScreen)null);
 							mc.setIngameFocus();
