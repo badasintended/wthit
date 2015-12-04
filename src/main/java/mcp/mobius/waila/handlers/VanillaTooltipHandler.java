55030df684d658609a8da83baf91183147744494
GirafiStudios <girafi0@gmail.com>
2015-12-04 03:19:56 +0100

    Bumped API version Fixed double showing of mod name on item tooltip when JEI is present

--- src/main/java/mcp/mobius/waila/handlers/VanillaTooltipHandler.java
+++ src/main/java/mcp/mobius/waila/handlers/VanillaTooltipHandler.java
@@ -2,6 +2,7 @@ package mcp.mobius.waila.handlers;
 
 import mcp.mobius.waila.utils.ModIdentification;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -11,7 +12,7 @@ public class VanillaTooltipHandler {
 	@SideOnly(Side.CLIENT)	
 	public void tooltipEvent(ItemTooltipEvent event) {
 		String canonicalName = ModIdentification.nameFromStack(event.itemStack);
-		if (canonicalName != null && !canonicalName.equals(""))
+		if (canonicalName != null && !canonicalName.equals("") && !Loader.isModLoaded("JEI"))
 			event.toolTip.add("\u00a79\u00a7o" +  canonicalName);
 	}
 }
