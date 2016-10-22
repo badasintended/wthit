400c71c62b159aefccc1ebfa3df5cd0292a15bc6
Nicholas Ignoffo <speedynutty68@gmail.com>
2016-10-22 00:16:37 -0700

    Native support for displaying tank info
    Disabled by default to avoid clashing with mods that already implement this. Limited to 5 displayed tanks to avoid mishaps where hundreds get added. *looks at enderio*

---
+++ src/main/java/mcp/mobius/waila/addons/capability/HUDHandlerTank.java
@@ -0,0 +1,65 @@
+package mcp.mobius.waila.addons.capability;
+
+import mcp.mobius.waila.api.IWailaConfigHandler;
+import mcp.mobius.waila.api.IWailaDataAccessor;
+import mcp.mobius.waila.api.IWailaDataProvider;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.translation.I18n;
+import net.minecraft.world.World;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidTankProperties;
+
+import java.util.List;
+
+public class HUDHandlerTank implements IWailaDataProvider {
+
+    @Override
+    public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        return null;
+    }
+
+    @Override
+    public List<String> getWailaHead(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        return null;
+    }
+
+    @Override
+    public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        if (!config.getConfig("capability.tankinfo"))
+            return currenttip;
+
+        TileEntity tile = accessor.getTileEntity();
+        if (tile == null || !tile.hasCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, accessor.getSide()))
+            return currenttip;
+
+        IFluidHandler fluidHandler = tile.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, accessor.getSide());
+        int fluidCount = 0;
+        for (IFluidTankProperties property : fluidHandler.getTankProperties()) {
+            if (property.getContents() != null) {
+                if (fluidCount <= 5) {
+                    currenttip.add(String.format("%s: %d / %d mB", property.getContents().getLocalizedName(), property.getContents().amount, property.getCapacity()));
+                    fluidCount++;
+                } else currenttip.add(I18n.translateToLocal("hud.msg.toomuch"));
+            }
+        }
+
+        return currenttip;
+    }
+
+    @Override
+    public List<String> getWailaTail(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        return null;
+    }
+
+    @Override
+    public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, BlockPos pos) {
+        if (te != null)
+            te.writeToNBT(tag);
+        return tag;
+    }
+}
---
+++ src/main/java/mcp/mobius/waila/addons/capability/PluginCapability.java
@@ -0,0 +1,17 @@
+package mcp.mobius.waila.addons.capability;
+
+import mcp.mobius.waila.api.IWailaPlugin;
+import mcp.mobius.waila.api.IWailaRegistrar;
+import mcp.mobius.waila.api.WailaPlugin;
+import net.minecraft.tileentity.TileEntity;
+
+@WailaPlugin
+public class PluginCapability implements IWailaPlugin {
+
+    @Override
+    public void register(IWailaRegistrar registrar) {
+        registrar.registerBodyProvider(new HUDHandlerTank(), TileEntity.class);
+
+        registrar.addConfig("Capability", "capability.tankinfo", false);
+    }
+}
