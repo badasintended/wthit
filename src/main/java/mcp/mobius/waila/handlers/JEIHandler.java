17f83b53dd538d8ea9503deb9e011573f811f65e
Nicholas Ignoffo <speedynutty68@gmail.com>
2016-10-22 23:47:07 -0700

    Feedback for when a block has no recipe/usage

--- src/main/java/mcp/mobius/waila/handlers/JEIHandler.java
+++ src/main/java/mcp/mobius/waila/handlers/JEIHandler.java
@@ -4,7 +4,11 @@ import mezz.jei.api.*;
 import mezz.jei.api.recipe.IFocus;
 import mezz.jei.gui.Focus;
 import mezz.jei.gui.ItemListOverlay;
+import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.util.text.TextFormatting;
 
 import javax.annotation.Nonnull;
 
@@ -20,11 +24,19 @@ public class JEIHandler extends BlankModPlugin {
 
     public static void displayRecipes(ItemStack stack) {
         runtime.getRecipesGui().show(new Focus<ItemStack>(IFocus.Mode.OUTPUT, stack));
+        if (Minecraft.getMinecraft().currentScreen != runtime.getRecipesGui()) {
+            Minecraft.getMinecraft().thePlayer.addChatComponentMessage(new TextComponentTranslation("client.msg.norecipe").setStyle(new Style().setColor(TextFormatting.RED)));
+            return;
+        }
         openItemList();
     }
 
     public static void displayUses(ItemStack stack) {
         runtime.getRecipesGui().show(new Focus<ItemStack>(IFocus.Mode.INPUT, stack));
+        if (Minecraft.getMinecraft().currentScreen != runtime.getRecipesGui()) {
+            Minecraft.getMinecraft().thePlayer.addChatComponentMessage(new TextComponentTranslation("client.msg.nousage").setStyle(new Style().setColor(TextFormatting.RED)));
+            return;
+        }
         openItemList();
     }
 
