ac7ee51003cb7d55f8a2185c60bda87f92328b98
Nicholas Ignoffo <speedynutty68@gmail.com>
2016-11-05 17:29:19 -0700

    Rewrite tank handler to be NBT based (#19)
    Breaks EnderIO support since they (for whatever reason) return false for `has/getCapability()` with a null facing even though that is perfectly valid.

--- src/main/java/mcp/mobius/waila/addons/capability/HUDHandlerTank.java
+++ src/main/java/mcp/mobius/waila/addons/capability/HUDHandlerTank.java
@@ -8,10 +8,12 @@ import mcp.mobius.waila.api.IWailaDataProvider;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.translation.I18n;
 import net.minecraft.world.World;
+import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.minecraftforge.fluids.capability.IFluidTankProperties;
@@ -28,18 +30,20 @@ public class HUDHandlerTank extends HUDHandlerBase {
             return currenttip;
 
         TileEntity tile = accessor.getTileEntity();
-        if (tile == null || !tile.hasCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, accessor.getSide()))
+        if (tile == null || !tile.hasCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, null))
             return currenttip;
 
-        IFluidHandler fluidHandler = tile.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, accessor.getSide());
+        NBTTagList tanks = accessor.getNBTData().getTagList("tankInfo", 10);
         int fluidCount = 0;
-        for (IFluidTankProperties property : fluidHandler.getTankProperties()) {
-            if (property.getContents() != null) {
-                if (fluidCount <= 5) {
-                    ((ITaggedList<String, String>) currenttip).add(String.format("%s: %d / %d mB", property.getContents().getLocalizedName(), property.getContents().amount, property.getCapacity()), "IFluidHandler");
-                    fluidCount++;
-                } else ((ITaggedList<String, String>) currenttip).add(I18n.translateToLocal("hud.msg.toomuch"), "IFluidHandler");
-            }
+        for (int i = 0; i < tanks.tagCount(); i++) {
+            NBTTagCompound tankInfo = tanks.getCompoundTagAt(i);
+            FluidStack contents = FluidStack.loadFluidStackFromNBT(tankInfo.getCompoundTag("fluid"));
+            int capacity = tankInfo.getInteger("capacity");
+
+            if (fluidCount <= 5) {
+                ((ITaggedList<String, String>) currenttip).add(String.format("%s: %d / %d mB", contents.getLocalizedName(), contents.amount, capacity), "IFluidHandler");
+                fluidCount++;
+            } else ((ITaggedList<String, String>) currenttip).add(I18n.translateToLocal("hud.msg.toomuch"), "IFluidHandler");
         }
 
         return currenttip;
@@ -47,8 +51,24 @@ public class HUDHandlerTank extends HUDHandlerBase {
 
     @Override
     public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, BlockPos pos) {
-        if (te != null)
+        if (te != null) {
             te.writeToNBT(tag);
+
+            if (te.hasCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, null)) {
+                NBTTagList tanks = new NBTTagList();
+                IFluidHandler fluidHandler = te.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, null);
+                for (IFluidTankProperties property : fluidHandler.getTankProperties()) {
+                    if (property.getContents() != null) {
+                        NBTTagCompound tankInfo = new NBTTagCompound();
+                        tankInfo.setTag("fluid", property.getContents().writeToNBT(new NBTTagCompound()));
+                        tankInfo.setInteger("capacity", property.getCapacity());
+                        tanks.appendTag(tankInfo);
+                    }
+                }
+
+                tag.setTag("tankInfo", tanks);
+            }
+        }
         return tag;
     }
 }
