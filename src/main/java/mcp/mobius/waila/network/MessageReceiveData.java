187219964bd64b8c091e408d31dd62d7fd510a95
Nicholas Ignoffo <speedynutty68@gmail.com>
2017-01-28 21:50:32 -0800

    Fix serializing NBT tags
    (cherry picked from commit d0540cf)

--- src/main/java/mcp/mobius/waila/network/MessageReceiveData.java
+++ src/main/java/mcp/mobius/waila/network/MessageReceiveData.java
@@ -2,10 +2,10 @@ package mcp.mobius.waila.network;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufInputStream;
+import io.netty.buffer.ByteBufOutputStream;
 import mcp.mobius.waila.api.impl.DataAccessorCommon;
 import net.minecraft.nbt.CompressedStreamTools;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraftforge.fml.common.network.ByteBufUtils;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
@@ -32,7 +32,11 @@ public class MessageReceiveData implements IMessage {
 
     @Override
     public void toBytes(ByteBuf buf) {
-        ByteBufUtils.writeTag(buf, tag);
+        try {
+            CompressedStreamTools.writeCompressed(tag, new ByteBufOutputStream(buf));
+        } catch (Exception e) {
+
+        }
     }
 
     public static class Handler implements IMessageHandler<MessageReceiveData, IMessage> {
