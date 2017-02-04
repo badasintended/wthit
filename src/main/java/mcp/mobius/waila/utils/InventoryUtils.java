dbb3d926f9a01230009b2ffe564e912f33bc1a60
Nicholas Ignoffo <speedynutty68@gmail.com>
2017-02-04 08:00:42 -0800

    Add missing class from cherry-pick

---
+++ src/main/java/mcp/mobius/waila/utils/InventoryUtils.java
@@ -0,0 +1,78 @@
+package mcp.mobius.waila.utils;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.IItemHandlerModifiable;
+import net.minecraftforge.items.ItemHandlerHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InventoryUtils {
+
+    public static NBTTagList invToNBT(IItemHandler itemHandler, int amount) {
+        NBTTagList nbtTagList = new NBTTagList();
+
+        List<ItemStack> compressed = compressInventory(itemHandler);
+        for (int i = 0; i < compressed.size(); i++) {
+            if (i > amount)
+                break;
+            ItemStack stack = compressed.get(i);
+            if (stack != null) {
+                NBTTagCompound itemTag = new NBTTagCompound();
+                itemTag.setInteger("Slot", i);
+                writeStack(stack, itemTag);
+                nbtTagList.appendTag(itemTag);
+            }
+        }
+
+        return nbtTagList;
+    }
+
+    public static void populateInv(IItemHandlerModifiable itemHandler, NBTTagList tagList) {
+        for (int i = 0; i < tagList.tagCount(); i++) {
+            NBTTagCompound itemTags = tagList.getCompoundTagAt(i);
+            int slot = itemTags.getInteger("Slot");
+
+            if (slot >= 0 && slot < itemHandler.getSlots())
+                itemHandler.setStackInSlot(slot, readStack(itemTags));
+        }
+    }
+
+    public static void writeStack(ItemStack stack, NBTTagCompound tagCompound) {
+        stack.writeToNBT(tagCompound);
+        tagCompound.setInteger("CountI", stack.stackSize);
+    }
+
+    public static ItemStack readStack(NBTTagCompound tagCompound) {
+        ItemStack stack = ItemStack.loadItemStackFromNBT(tagCompound);
+        stack.stackSize = tagCompound.getInteger("CountI");
+        return stack;
+    }
+
+    public static List<ItemStack> compressInventory(IItemHandler itemHandler) {
+        List<ItemStack> compressed = new ArrayList<ItemStack>();
+        for (int slot = 0; slot < itemHandler.getSlots(); slot++) {
+            ItemStack stack = itemHandler.getStackInSlot(slot);
+            if (stack == null)
+                continue;
+
+            addStack(compressed, stack);
+        }
+
+        return compressed;
+    }
+
+    public static void addStack(List<ItemStack> stacks, ItemStack stack) {
+        for (ItemStack invStack : stacks) {
+            if (ItemHandlerHelper.canItemStacksStack(invStack, stack)) {
+                invStack.stackSize += stack.stackSize;
+                return;
+            }
+        }
+
+        stacks.add(stack.copy());
+    }
+}
