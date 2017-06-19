43772a92505191f68dcf8c0ccda3127ee5a0f26c
Nicholas Ignoffo <speedynutty68@gmail.com>
2017-06-18 21:04:47 -0700

    Remove alternate crop option
    Did more harm than good
    
    (cherry picked from commit 2c028e2)

--- src/main/java/mcp/mobius/waila/addons/minecraft/HUDHandlerVanilla.java
+++ src/main/java/mcp/mobius/waila/addons/minecraft/HUDHandlerVanilla.java
@@ -18,14 +18,11 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.translation.I18n;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
-import java.lang.reflect.Method;
+import javax.annotation.Nonnull;
 import java.util.List;
 
 public class HUDHandlerVanilla extends HUDHandlerBase {
-
-    static Method getCrop;
 
     static Block mobSpawner = Blocks.MOB_SPAWNER;
     static Block crops = Blocks.WHEAT;
@@ -45,7 +42,7 @@ public class HUDHandlerVanilla extends HUDHandlerBase {
     static Block silverfish = Blocks.MONSTER_EGG;
     static Block flowerpot = Blocks.FLOWER_POT;
 
-
+    @Nonnull
     @Override
     public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler config) {
         Block block = accessor.getBlock();
@@ -130,26 +127,13 @@ public class HUDHandlerVanilla extends HUDHandlerBase {
             }
         }
 
-        if (config.getConfig("vanilla.alternatecropitem")) {
-            if (block instanceof BlockCrops) {
-                if (getCrop == null)
-                    getCrop = ReflectionHelper.findMethod(BlockCrops.class, null, new String[]{"getCrop", "func_149865_P"});
+        // Wheat crop should display Wheat item
+        if (block == crops)
+            return new ItemStack(Items.WHEAT);
 
-                try {
-                    return new ItemStack((Item) getCrop.invoke(block), 1, block.damageDropped(accessor.getBlockState()));
-                } catch (Exception e) {
-                    return ItemStack.EMPTY;
-                }
-            }
-        } else {
-            // Wheat crop should display Wheat item
-            if (block == crops)
-                return new ItemStack(Items.WHEAT);
-
-            // Beetroot crop should display Beetroot item
-            if (block == beet)
-                return new ItemStack(Items.BEETROOT);
-        }
+        // Beetroot crop should display Beetroot item
+        if (block == beet)
+            return new ItemStack(Items.BEETROOT);
 
         // Display farmland instead of dirt
         if (block == farmland) {
@@ -159,6 +143,7 @@ public class HUDHandlerVanilla extends HUDHandlerBase {
         return ItemStack.EMPTY;
     }
 
+    @Nonnull
     @Override
     public List<String> getWailaHead(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
         Block block = accessor.getBlock();
@@ -189,6 +174,7 @@ public class HUDHandlerVanilla extends HUDHandlerBase {
         return currenttip;
     }
 
+    @Nonnull
     @Override
     public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
         Block block = accessor.getBlock();
@@ -289,6 +275,7 @@ public class HUDHandlerVanilla extends HUDHandlerBase {
         return currenttip;
     }
 
+    @Nonnull
     @Override
     public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, BlockPos pos) {
         return te.writeToNBT(tag);
@@ -301,7 +288,6 @@ public class HUDHandlerVanilla extends HUDHandlerBase {
         registrar.addConfig("VanillaMC", "vanilla.comparator");
         registrar.addConfig("VanillaMC", "vanilla.redstone");
         registrar.addConfig("VanillaMC", "vanilla.silverfish");
-        registrar.addConfig("VanillaMC", "vanilla.alternatecropitem");
         registrar.addConfigRemote("VanillaMC", "vanilla.jukebox");
 
         IWailaDataProvider provider = new HUDHandlerVanilla();
