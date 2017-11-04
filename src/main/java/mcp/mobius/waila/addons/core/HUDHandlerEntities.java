12feded3006649aabb229002d8341bb161d8f04d
Nicholas Ignoffo <nick@tehnut.info>
2017-11-03 22:21:21 -0700

    Fix entity mod name displaying modid

--- src/main/java/mcp/mobius/waila/addons/core/HUDHandlerEntities.java
+++ src/main/java/mcp/mobius/waila/addons/core/HUDHandlerEntities.java
@@ -5,16 +5,13 @@ import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaEntityAccessor;
 import mcp.mobius.waila.api.IWailaEntityProvider;
 import mcp.mobius.waila.config.FormattingConfig;
+import mcp.mobius.waila.utils.ModIdentification;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.text.translation.I18n;
-import net.minecraft.world.World;
 import net.minecraftforge.fml.common.ModContainer;
 import net.minecraftforge.fml.common.registry.EntityEntry;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
-import net.minecraftforge.fml.common.registry.EntityRegistry.EntityRegistration;
 
 import javax.annotation.Nonnull;
 import java.util.List;
@@ -73,6 +70,7 @@ public class HUDHandlerEntities implements IWailaEntityProvider {
         if (entityEntry == null)
             return "Unknown";
 
-        return entityEntry.getRegistryName().getResourceDomain();
+        ModContainer container = ModIdentification.findModContainer(entityEntry.getRegistryName().getResourceDomain());
+        return container.getName();
     }
 }
