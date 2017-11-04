9ebbc390bdad4f435df16f961bfc7b83f8a5a0e9
Nicholas Ignoffo <nick@tehnut.info>
2017-11-03 22:21:21 -0700

    Fix entity mod name displaying modid
    (cherry picked from commit 12feded)

--- src/main/java/mcp/mobius/waila/addons/core/HUDHandlerEntities.java
+++ src/main/java/mcp/mobius/waila/addons/core/HUDHandlerEntities.java
@@ -5,12 +5,14 @@ import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaEntityAccessor;
 import mcp.mobius.waila.api.IWailaEntityProvider;
 import mcp.mobius.waila.config.FormattingConfig;
+import mcp.mobius.waila.utils.ModIdentification;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.text.translation.I18n;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.ModContainer;
 import net.minecraftforge.fml.common.registry.EntityEntry;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
 
@@ -88,6 +90,7 @@ public class HUDHandlerEntities implements IWailaEntityProvider {
         if (entityEntry == null)
             return "Unknown";
 
-        return entityEntry.getRegistryName().getResourceDomain();
+        ModContainer container = ModIdentification.findModContainer(entityEntry.getRegistryName().getResourceDomain());
+        return container.getName();
     }
 }
