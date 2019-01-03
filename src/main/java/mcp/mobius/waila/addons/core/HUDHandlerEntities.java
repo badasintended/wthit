377c4ed50379f9724430bb4b1abdadf7655c290d
Nicholas Ignoffo <nick@tehnut.info>
2019-01-02 19:56:03 -0800

    Fix entity names

--- src/main/java/mcp/mobius/waila/addons/core/HUDHandlerEntities.java
+++ src/main/java/mcp/mobius/waila/addons/core/HUDHandlerEntities.java
@@ -19,7 +19,7 @@ public class HUDHandlerEntities implements IEntityComponentProvider {
 
     @Override
     public void appendHead(List<TextComponent> tooltip, IEntityAccessor accessor, IPluginConfig config) {
-        ((ITaggedList<TextComponent, Identifier>) tooltip).add(new StringTextComponent(String.format(Waila.config.getFormatting().getEntityName(), accessor.getEntity().getDisplayName())), HUDHandlerBlocks.OBJECT_NAME_TAG);
+        ((ITaggedList<TextComponent, Identifier>) tooltip).add(new StringTextComponent(String.format(Waila.config.getFormatting().getEntityName(), accessor.getEntity().getDisplayName().getFormattedText())), HUDHandlerBlocks.OBJECT_NAME_TAG);
         if (config.get(PluginCore.CONFIG_SHOW_REGISTRY))
             ((ITaggedList<TextComponent, Identifier>) tooltip).add(new StringTextComponent(Registry.ENTITY_TYPE.getId(accessor.getEntity().getType()).toString()).setStyle(new Style().setColor(TextFormat.GRAY)), HUDHandlerBlocks.REGISTRY_NAME_TAG);
     }
