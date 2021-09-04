2e25acda6b5ab8bc71264e362df12d0ddddb2f46
deirn <deirn@bai.lol>
2021-09-04 14:09:55 +0700

    fix server crash

--- common/src/main/java/mcp/mobius/waila/config/ConfigEntry.java
+++ common/src/main/java/mcp/mobius/waila/config/ConfigEntry.java
@@ -1,77 +1,17 @@
 package mcp.mobius.waila.config;
 
-import java.util.function.Consumer;
-
 import com.google.common.base.Preconditions;
 import com.google.gson.JsonPrimitive;
-import mcp.mobius.waila.gui.widget.value.BooleanValue;
-import mcp.mobius.waila.gui.widget.value.ConfigValue;
-import mcp.mobius.waila.gui.widget.value.EnumValue;
-import mcp.mobius.waila.gui.widget.value.InputValue;
 import net.minecraft.resources.ResourceLocation;
 
-@SuppressWarnings({"unchecked", "rawtypes"})
+@SuppressWarnings({"rawtypes"})
 public class ConfigEntry<T> {
 
-    public static final Type<Boolean> BOOLEAN = new Type<>() {
-        @Override
-        public Boolean parseValue(JsonPrimitive element, Boolean defaultValue) {
-            return element.getAsBoolean();
-        }
-
-        @Override
-        public ConfigValue<Boolean> createConfigValue(String name, Boolean value, Consumer<Boolean> consumer) {
-            return new BooleanValue(name, value, consumer);
-        }
-    };
-
-    public static final Type<Integer> INTEGER = new Type<>() {
-        @Override
-        public Integer parseValue(JsonPrimitive element, Integer defaultValue) {
-            return element.getAsInt();
-        }
-
-        @Override
-        public ConfigValue<Integer> createConfigValue(String name, Integer value, Consumer<Integer> consumer) {
-            return new InputValue<>(name, value, consumer, InputValue.INTEGER);
-        }
-    };
-
-    public static final Type<Double> DOUBLE = new Type<>() {
-        @Override
-        public Double parseValue(JsonPrimitive element, Double defaultValue) {
-            return element.getAsDouble();
-        }
-
-        @Override
-        public ConfigValue<Double> createConfigValue(String name, Double value, Consumer<Double> consumer) {
-            return new InputValue<>(name, value, consumer, InputValue.DECIMAL);
-        }
-    };
-
-    public static final Type<String> STRING = new Type<>() {
-        @Override
-        public String parseValue(JsonPrimitive element, String defaultValue) {
-            return element.getAsString();
-        }
-
-        @Override
-        public ConfigValue<String> createConfigValue(String name, String value, Consumer<String> consumer) {
-            return new InputValue<>(name, value, consumer);
-        }
-    };
-
-    public static final Type<Enum<? extends Enum>> ENUM = new Type<>() {
-        @Override
-        public Enum<?> parseValue(JsonPrimitive element, Enum<?> defaultValue) {
-            return Enum.valueOf(defaultValue.getDeclaringClass(), element.getAsString());
-        }
-
-        @Override
-        public ConfigValue<Enum<?>> createConfigValue(String name, Enum<?> value, Consumer<Enum<?>> consumer) {
-            return new EnumValue(name, value.getDeclaringClass().getEnumConstants(), value, consumer);
-        }
-    };
+    public static final Type<Boolean> BOOLEAN = (e, d) -> e.getAsBoolean();
+    public static final Type<Integer> INTEGER = (e, d) -> e.getAsInt();
+    public static final Type<Double> DOUBLE = (e, d) -> e.getAsDouble();
+    public static final Type<String> STRING = (e, d) -> e.getAsString();
+    public static final Type<Enum<? extends Enum>> ENUM = (e, d) -> Enum.valueOf(d.getDeclaringClass(), e.getAsString());
 
     private final ResourceLocation id;
     private final T defaultValue;
@@ -117,8 +57,6 @@ public class ConfigEntry<T> {
     public interface Type<T> {
 
         T parseValue(JsonPrimitive element, T defaultValue);
-
-        ConfigValue<T> createConfigValue(String name, T value, Consumer<T> consumer);
 
         default ConfigEntry<T> create(ResourceLocation id, T defaultValue, boolean synced) {
             return new ConfigEntry<>(id, defaultValue, synced, this);
--- common/src/main/java/mcp/mobius/waila/gui/screen/PluginConfigScreen.java
+++ common/src/main/java/mcp/mobius/waila/gui/screen/PluginConfigScreen.java
@@ -1,27 +1,49 @@
 package mcp.mobius.waila.gui.screen;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
+import java.util.function.Consumer;
 
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import mcp.mobius.waila.config.ConfigEntry;
 import mcp.mobius.waila.config.PluginConfig;
 import mcp.mobius.waila.gui.widget.ConfigListWidget;
+import mcp.mobius.waila.gui.widget.value.BooleanValue;
+import mcp.mobius.waila.gui.widget.value.ConfigValue;
+import mcp.mobius.waila.gui.widget.value.EnumValue;
+import mcp.mobius.waila.gui.widget.value.InputValue;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 
+@SuppressWarnings({"unchecked", "rawtypes"})
 public class PluginConfigScreen extends ConfigScreen {
 
     private static final String NO_CATEGORY = "no_category";
+    private static final Map<ConfigEntry.Type<Object>, ConfigValueFunction<Object>> ENTRY_TO_VALUE = new HashMap<>();
+
+    static {
+        register(ConfigEntry.BOOLEAN, BooleanValue::new);
+        register(ConfigEntry.INTEGER, (name, value, save) -> new InputValue<>(name, value, save, InputValue.INTEGER));
+        register(ConfigEntry.DOUBLE, (name, value, save) -> new InputValue<>(name, value, save, InputValue.DECIMAL));
+        register(ConfigEntry.STRING, InputValue::new);
+        register(ConfigEntry.ENUM, (name, value, save) -> new EnumValue(name, value.getDeclaringClass().getEnumConstants(), value, save));
+    }
 
     public PluginConfigScreen(Screen parent) {
         super(parent, new TranslatableComponent("gui.waila.plugin_settings"), PluginConfig.INSTANCE::save, PluginConfig.INSTANCE::reload);
     }
 
+    private static <T> void register(ConfigEntry.Type<T> type, ConfigValueFunction<T> function) {
+        ENTRY_TO_VALUE.put((ConfigEntry.Type<Object>) type, (ConfigValueFunction<Object>) function);
+    }
+
     @Override
+    @SuppressWarnings("ConstantConditions")
     public ConfigListWidget getOptions() {
         ConfigListWidget options = new ConfigListWidget(this, minecraft, width, height, 32, height - 32, 30, PluginConfig.INSTANCE::save);
         for (String namespace : PluginConfig.INSTANCE.getNamespaces()) {
@@ -53,13 +75,20 @@ public class PluginConfigScreen extends ConfigScreen {
                         }
                         int index = categories.getInt(category);
                         categories.put(category, index + 1);
-                        options.with(index, entry.getType().createConfigValue(translationKey + "." + path, entry.getValue(), entry::setValue));
+                        options.with(index, ENTRY_TO_VALUE.get(entry.getType()).create(translationKey + "." + path, entry.getValue(), entry::setValue));
                     }
                     return options;
                 }
             }));
         }
         return options;
+    }
+
+    @FunctionalInterface
+    private interface ConfigValueFunction<T> {
+
+        ConfigValue<T> create(String name, T value, Consumer<T> save);
+
     }
 
 }
