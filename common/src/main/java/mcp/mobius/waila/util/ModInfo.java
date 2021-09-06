60f45f14e486b57bcf5fa1b29f2f895078e64421
deirn <deirn@bai.lol>
2021-09-06 14:30:09 +0700

    make ModInfo a class

--- common/src/main/java/mcp/mobius/waila/util/ModInfo.java
+++ common/src/main/java/mcp/mobius/waila/util/ModInfo.java
@@ -7,13 +7,21 @@ import java.util.function.Function;
 
 import mcp.mobius.waila.api.IModInfo;
 
-public record ModInfo(String id, String name) implements IModInfo {
+public final class ModInfo implements IModInfo {
 
     private static final Map<String, ModInfo> CONTAINER_CACHE = new HashMap<>();
     public static Function<String, Optional<ModInfo>> supplier;
 
     static {
         register(new ModInfo("minecraft", "Minecraft"));
+    }
+
+    private final String id;
+    private final String name;
+
+    public ModInfo(String id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
     public static void register(ModInfo info) {
