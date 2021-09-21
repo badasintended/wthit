b7acb0da9728266a45667d5e708f38d7a54e80ae
deirn <deirn@bai.lol>
2021-09-21 15:29:14 +0700

    don't send furnace data if not lit

--- src/pluginVanilla/java/mcp/mobius/waila/plugin/vanilla/component/FurnaceComponent.java
+++ src/pluginVanilla/java/mcp/mobius/waila/plugin/vanilla/component/FurnaceComponent.java
@@ -11,8 +11,8 @@ import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.FurnaceBlock;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 
 import static mcp.mobius.waila.plugin.vanilla.renderer.Renderers.ITEM;
 import static mcp.mobius.waila.plugin.vanilla.renderer.Renderers.PROGRESS;
@@ -23,31 +23,29 @@ public enum FurnaceComponent implements IBlockComponentProvider, IServerDataProv
 
     @Override
     public void appendBody(ITooltip tooltip, IBlockAccessor accessor, IPluginConfig config) {
-        if (!config.getBoolean(Options.FURNACE_CONTENTS))
-            return;
+        if (config.getBoolean(Options.FURNACE_CONTENTS) && accessor.getServerData().contains("progress")) {
+            ListTag furnaceItems = accessor.getServerData().getList("furnace", Tag.TAG_COMPOUND);
 
-        if (!accessor.getBlockState().getValue(BlockStateProperties.LIT))
-            return;
-
-        ListTag furnaceItems = accessor.getServerData().getList("furnace", Tag.TAG_COMPOUND);
-
-        tooltip.addDrawable()
-            .with(ITEM, furnaceItems.getCompound(0))
-            .with(ITEM, furnaceItems.getCompound(1))
-            .with(PROGRESS, accessor.getServerData())
-            .with(ITEM, furnaceItems.getCompound(2));
+            tooltip.addDrawable()
+                .with(ITEM, furnaceItems.getCompound(0))
+                .with(ITEM, furnaceItems.getCompound(1))
+                .with(PROGRESS, accessor.getServerData())
+                .with(ITEM, furnaceItems.getCompound(2));
+        }
     }
 
     @Override
-    public void appendServerData(CompoundTag data, ServerPlayer player, Level world, AbstractFurnaceBlockEntity furnaceBlockEntity) {
-        ListTag items = new ListTag();
-        items.add(furnaceBlockEntity.getItem(0).save(new CompoundTag()));
-        items.add(furnaceBlockEntity.getItem(1).save(new CompoundTag()));
-        items.add(furnaceBlockEntity.getItem(2).save(new CompoundTag()));
-        data.put("furnace", items);
+    public void appendServerData(CompoundTag data, ServerPlayer player, Level world, AbstractFurnaceBlockEntity furnace) {
+        if (furnace.getBlockState().getValue(FurnaceBlock.LIT)) {
+            ListTag items = new ListTag();
+            items.add(furnace.getItem(0).save(new CompoundTag()));
+            items.add(furnace.getItem(1).save(new CompoundTag()));
+            items.add(furnace.getItem(2).save(new CompoundTag()));
+            data.put("furnace", items);
 
-        data.putInt("progress", furnaceBlockEntity.cookingProgress);
-        data.putInt("total", furnaceBlockEntity.cookingTotalTime);
+            data.putInt("progress", furnace.cookingProgress);
+            data.putInt("total", furnace.cookingTotalTime);
+        }
     }
 
 }
