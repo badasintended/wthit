8eec3a2a55f85272e509fb511a526922b4a0ab09
deirn <deirn@bai.lol>
2021-10-24 00:23:06 +0700

    e

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,4 +13,7 @@ dependencies {
     implementation("gradle.plugin.com.matthewprenger:CurseGradle:1.4.0") {
         exclude(group = "com.google.guava")
     }
+
+    // fixes clashes with loom's guava that makes access widener fails to be read
+    implementation("com.google.guava:guava:31.0.1-jre")
 }
