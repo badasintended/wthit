6629c75dd2a120daa70a7741436e5779a2c81473
deirn <deirn@bai.lol>
2022-05-15 19:14:50 +0700

    fix dump generator outputting entity provider classes on block part

--- src/main/java/mcp/mobius/waila/debug/DumpGenerator.java
+++ src/main/java/mcp/mobius/waila/debug/DumpGenerator.java
@@ -46,10 +46,10 @@ public class DumpGenerator {
 
         builder.append("\n## Block");
         createSection(builder, "Override Providers", registrar.blockOverride);
-        createSection(builder, "Display Item Providers", registrar.entityIcon);
-        createSection(builder, "Head Providers", registrar.entityComponent.get(HEAD));
-        createSection(builder, "Body Providers", registrar.entityComponent.get(BODY));
-        createSection(builder, "Tail Providers", registrar.entityComponent.get(TAIL));
+        createSection(builder, "Display Item Providers", registrar.blockIcon);
+        createSection(builder, "Head Providers", registrar.blockComponent.get(HEAD));
+        createSection(builder, "Body Providers", registrar.blockComponent.get(BODY));
+        createSection(builder, "Tail Providers", registrar.blockComponent.get(TAIL));
         createSection(builder, "Data Providers", registrar.blockData);
 
         builder.append("\n## Entity");
