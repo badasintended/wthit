c6340161586faaba56fd96b6ab99c13d8bc6b031
deirn <deirn@bai.lol>
2022-06-19 14:34:12 +0700

    use badasintended/maven repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,7 +60,7 @@ subprojects {
     publishing {
         repositories {
             maven {
-                url = uri("https://maven.pkg.github.com/badasintended/wthit")
+                url = uri("https://maven.pkg.github.com/badasintended/maven")
                 name = "GitHub"
                 credentials {
                     username = env["GITHUB_ACTOR"]
