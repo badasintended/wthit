229d2884d6d6a2e55bbcbfd303560dd57bab9574
deirn <deirn@bai.lol>
2022-07-25 21:36:16 +0700

    add option to disable "Sneak for details" text

--- src/api/java/mcp/mobius/waila/api/IWailaConfig.java
+++ src/api/java/mcp/mobius/waila/api/IWailaConfig.java
@@ -25,6 +25,8 @@ public interface IWailaConfig {
 
         boolean isShiftForDetails();
 
+        boolean isHideShiftText();
+
         DisplayMode getDisplayMode();
 
         boolean isHideFromPlayerList();
--- src/main/java/mcp/mobius/waila/config/WailaConfig.java
+++ src/main/java/mcp/mobius/waila/config/WailaConfig.java
@@ -50,6 +50,7 @@ public class WailaConfig implements IWailaConfig {
 
         private boolean displayTooltip = true;
         private boolean shiftForDetails = false;
+        private boolean hideShiftText = false;
         private DisplayMode displayMode = DisplayMode.TOGGLE;
         private boolean hideFromPlayerList = true;
         private boolean hideFromDebug = true;
@@ -74,6 +75,15 @@ public class WailaConfig implements IWailaConfig {
 
         public void setShiftForDetails(boolean shiftForDetails) {
             this.shiftForDetails = shiftForDetails;
+        }
+
+        @Override
+        public boolean isHideShiftText() {
+            return hideShiftText;
+        }
+
+        public void setHideShiftText(boolean hideShiftText) {
+            this.hideShiftText = hideShiftText;
         }
 
         @Override
