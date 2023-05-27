9ff50ea8eab0029d45e28b8b80305e9d1ddf113c
deirn <deirn@bai.lol>
2023-05-27 11:09:51 +0700

    nbt data test

---
+++ src/pluginTest/java/mcp/mobius/waila/plugin/test/NbtDataTest.java
@@ -0,0 +1,38 @@
+package mcp.mobius.waila.plugin.test;
+
+import mcp.mobius.waila.api.IBlockAccessor;
+import mcp.mobius.waila.api.IBlockComponentProvider;
+import mcp.mobius.waila.api.IDataProvider;
+import mcp.mobius.waila.api.IDataWriter;
+import mcp.mobius.waila.api.IPluginConfig;
+import mcp.mobius.waila.api.IServerAccessor;
+import mcp.mobius.waila.api.ITooltip;
+import mcp.mobius.waila.api.component.PairComponent;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.entity.FurnaceBlockEntity;
+
+public enum NbtDataTest implements IBlockComponentProvider, IDataProvider<FurnaceBlockEntity> {
+
+    INSTANCE;
+
+    public static final ResourceLocation ENABLED = new ResourceLocation("test:data.nbt");
+
+    @Override
+    public void appendBody(ITooltip tooltip, IBlockAccessor accessor, IPluginConfig config) {
+        CompoundTag data = accessor.getData().raw();
+        if (data.contains("testnbt")) {
+            int value = data.getInt("testnbt");
+            tooltip.addLine(new PairComponent(Component.literal("testnbt"), Component.literal(String.valueOf(value))));
+        }
+    }
+
+    @Override
+    public void appendData(IDataWriter data, IServerAccessor<FurnaceBlockEntity> accessor, IPluginConfig config) {
+        if (config.getBoolean(ENABLED)) {
+            data.raw().putInt("testnbt", 2421);
+        }
+    }
+
+}
