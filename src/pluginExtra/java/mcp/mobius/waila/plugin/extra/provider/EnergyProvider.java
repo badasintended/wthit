8cb676347421e00a37b93bba5d52e27093473e1a
deirn <deirn@bai.lol>
2023-07-31 21:47:01 +0700

    add ways to easily mark object containing infinite energy

--- src/pluginExtra/java/mcp/mobius/waila/plugin/extra/provider/EnergyProvider.java
+++ src/pluginExtra/java/mcp/mobius/waila/plugin/extra/provider/EnergyProvider.java
@@ -1,15 +1,28 @@
 package mcp.mobius.waila.plugin.extra.provider;
 
+import mcp.mobius.waila.api.IDataProvider;
+import mcp.mobius.waila.api.IDataWriter;
 import mcp.mobius.waila.api.IPluginConfig;
+import mcp.mobius.waila.api.IRegistrar;
+import mcp.mobius.waila.api.IServerAccessor;
 import mcp.mobius.waila.api.ITooltip;
+import mcp.mobius.waila.api.WailaConstants;
 import mcp.mobius.waila.api.WailaHelper;
 import mcp.mobius.waila.api.component.BarComponent;
 import mcp.mobius.waila.api.component.PairComponent;
 import mcp.mobius.waila.api.component.WrappedComponent;
 import mcp.mobius.waila.api.data.EnergyData;
 import mcp.mobius.waila.plugin.extra.data.EnergyDescription;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 
 public class EnergyProvider extends DataProvider<EnergyData> {
 
@@ -17,8 +30,19 @@ public class EnergyProvider extends DataProvider<EnergyData> {
 
     private static final String INFINITE = "∞";
 
+    private static final ResourceLocation INFINITE_TAG_ID = new ResourceLocation(WailaConstants.NAMESPACE, "extra/infinite_energy");
+    private static final TagKey<Block> INFINITE_BLOCK_TAG = TagKey.create(Registries.BLOCK, INFINITE_TAG_ID);
+    private static final TagKey<BlockEntityType<?>> INFINITE_BLOCK_ENTITY_TAG = TagKey.create(Registries.BLOCK_ENTITY_TYPE, INFINITE_TAG_ID);
+    private static final TagKey<EntityType<?>> INFINITE_ENTITY_TAG = TagKey.create(Registries.ENTITY_TYPE, INFINITE_TAG_ID);
+
     private EnergyProvider() {
         super(EnergyData.ID, EnergyData.class, EnergyData::new);
+    }
+
+    @Override
+    protected void registerAdditions(IRegistrar registrar, int priority) {
+        registrar.addBlockData(new InfiniteEnergyBlockProvider(), BlockEntity.class, 1);
+        registrar.addEntityData(new InfiniteEnergyEntityProvider(), Entity.class, 1);
     }
 
     @Override
@@ -45,6 +69,35 @@ public class EnergyProvider extends DataProvider<EnergyData> {
         tooltip.setLine(EnergyData.ID, new PairComponent(
             new WrappedComponent(name),
             new BarComponent(ratio, 0xFF000000 | color, text)));
+    }
+
+    private static class InfiniteEnergyBlockProvider implements IDataProvider<BlockEntity> {
+
+        @Override
+        public void appendData(IDataWriter data, IServerAccessor<BlockEntity> accessor, IPluginConfig config) {
+            data.add(EnergyData.class, res -> {
+                BlockEntity target = accessor.getTarget();
+
+                if (target.getBlockState().is(INFINITE_BLOCK_TAG)
+                    || BuiltInRegistries.BLOCK_ENTITY_TYPE.wrapAsHolder(target.getType()).is(INFINITE_BLOCK_ENTITY_TAG)) {
+                    res.add(EnergyData.INFINITE);
+                }
+            });
+        }
+
+    }
+
+    private static class InfiniteEnergyEntityProvider implements IDataProvider<Entity> {
+
+        @Override
+        public void appendData(IDataWriter data, IServerAccessor<Entity> accessor, IPluginConfig config) {
+            data.add(EnergyData.class, res -> {
+                if (accessor.getTarget().getType().is(INFINITE_ENTITY_TAG)) {
+                    res.add(EnergyData.INFINITE);
+                }
+            });
+        }
+
     }
 
 }
