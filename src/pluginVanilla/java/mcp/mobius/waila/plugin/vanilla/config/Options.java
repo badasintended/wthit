6ffbef2eaf9e4b4bc09cec7e95fab8057c373ee4
deirn <deirn@bai.lol>
2023-08-25 15:28:44 +0700

    add panda genes tooltip closes #178

--- src/pluginVanilla/java/mcp/mobius/waila/plugin/vanilla/config/Options.java
+++ src/pluginVanilla/java/mcp/mobius/waila/plugin/vanilla/config/Options.java
@@ -43,6 +43,7 @@ public final class Options {
     public static final ResourceLocation ATTRIBUTE_LONG_ARMOR_MAX      = rl("attribute.long_armor_max");
     public static final ResourceLocation ATTRIBUTE_HORSE_JUMP_HEIGHT   = rl("attribute.horse_jump_height");
     public static final ResourceLocation ATTRIBUTE_HORSE_SPEED         = rl("attribute.horse_speed");
+    public static final ResourceLocation ATTRIBUTE_PANDA_GENES         = rl("attribute.panda_genes");
     public static final ResourceLocation BOOK_BOOKSHELF                = rl("book.bookshelf");
     public static final ResourceLocation BOOK_WRITTEN                  = rl("book.written");
     public static final ResourceLocation BOOK_ENCHANTMENT_DISPLAY_MODE = rl("book.enchantment");
