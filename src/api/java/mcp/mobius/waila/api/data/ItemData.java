531a9f8019a914887608dd173589363bd7694499
deirn <deirn@bai.lol>
2023-09-20 17:06:06 +0700

    add option to not sort item list

--- src/api/java/mcp/mobius/waila/api/data/ItemData.java
+++ src/api/java/mcp/mobius/waila/api/data/ItemData.java
@@ -22,6 +22,7 @@ public final class ItemData implements IData {
     public static final ResourceLocation ID = BuiltinDataUtil.rl("item");
     public static final ResourceLocation CONFIG_SYNC_NBT = BuiltinDataUtil.rl("item.nbt");
     public static final ResourceLocation CONFIG_MAX_HEIGHT = BuiltinDataUtil.rl("item.max_height");
+    public static final ResourceLocation CONFIG_SORT_BY_COUNT = BuiltinDataUtil.rl("item.sort_by_count");
 
     /**
      * Creates an item data based from plugin config.
