eb7b73c72093bad8fd1db04ac649142d84eb43f9
deirn <deirn@bai.lol>
2023-11-23 12:18:37 +0700

    add beacon effects tooltip

--- src/pluginVanilla/java/mcp/mobius/waila/plugin/vanilla/config/Options.java
+++ src/pluginVanilla/java/mcp/mobius/waila/plugin/vanilla/config/Options.java
@@ -44,6 +44,7 @@ public final class Options {
     public static final ResourceLocation ATTRIBUTE_HORSE_JUMP_HEIGHT   = rl("attribute.horse_jump_height");
     public static final ResourceLocation ATTRIBUTE_HORSE_SPEED         = rl("attribute.horse_speed");
     public static final ResourceLocation ATTRIBUTE_PANDA_GENES         = rl("attribute.panda_genes");
+    public static final ResourceLocation ATTRIBUTE_BEACON_EFFECTS      = rl("attribute.beacon_effects");
     public static final ResourceLocation BOOK_BOOKSHELF                = rl("book.bookshelf");
     public static final ResourceLocation BOOK_WRITTEN                  = rl("book.written");
     public static final ResourceLocation BOOK_ENCHANTMENT_DISPLAY_MODE = rl("book.enchantment");
