c34e2c539d3a0aa877053bf81037dc13e7b9cd89
deirn <deirn@bai.lol>
2024-01-05 17:37:56 +0700

    disable overlay fps option
    (cherry picked from commit 0d3637a3861523e0b78f2216a0f61a641500076d)

--- src/main/java/mcp/mobius/waila/gui/hud/TooltipRenderer.java
+++ src/main/java/mcp/mobius/waila/gui/hud/TooltipRenderer.java
@@ -198,7 +198,9 @@ public class TooltipRenderer {
 
         var fps = state.getFps();
 
-        if (fps == 0) {
+        // TODO: Figure out why opacity not working properly
+        //noinspection ConstantValue
+        if (true) {
             render0(client, matrices, delta);
             return;
         }
