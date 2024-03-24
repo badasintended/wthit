625ceb4702c1fe635564a128935765398112479d
deirn <deirn@bai.lol>
2024-03-24 23:13:45 +0700

    Revert "use strict gradle plugin version"
    This reverts commit e7d58b52b4d8c4d2d7420b2fcb57b791658a5088.

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,14 +58,6 @@ allprojects {
             }
         }
     }
-
-    task("listPluginVersions") {
-        doLast {
-            project.plugins.forEach {
-                println("$it -> ${it.javaClass.protectionDomain.codeSource.location.toURI().toString().lowercase()}")
-            }
-        }
-    }
 }
 
 subprojects {
