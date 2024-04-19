1cf6373e13952e2e7748bc8339ab81917e4952a5
deirn <deirn@bai.lol>
2024-04-20 00:58:32 +0700

    fix quilt closes #255

--- platform/quilt/build.gradle.kts
+++ platform/quilt/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     modCompileRuntime("org.quiltmc.quilted-fabric-api:fabric-key-binding-api-v1:${rootProp["qfapi"]}")
     modCompileRuntime("org.quiltmc.quilted-fabric-api:fabric-rendering-v1:${rootProp["qfapi"]}")
     modCompileRuntime("org.quiltmc.quilted-fabric-api:fabric-lifecycle-events-v1:${rootProp["qfapi"]}")
+    modCompileRuntime("org.quiltmc.quilted-fabric-api:fabric-mining-level-api-v1:${rootProp["qfapi"]}")
 
     modCompileRuntime("com.terraformersmc:modmenu:${rootProp["modMenu"]}")
 
