f76ec70ca3cab02ffe1e981641d7fbe3b225ed06
deirn <deirn@bai.lol>
2024-04-20 22:07:53 +0700

    drop srgutils override
    (cherry picked from commit 02fca34d4e75feedb27681b6b317475de6b6713e)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -20,8 +20,4 @@ dependencies {
     implementation("com.squareup:javapoet:1.13.0")
     implementation("com.google.code.gson:gson:2.9.0")
     implementation("com.google.guava:guava:31.0.1-jre")
-
-    // https://github.com/MinecraftForge/ForgeGradle/issues/919
-    // The issue still happens because FART still uses outdated srgutils version
-    implementation("net.minecraftforge:srgutils:0.5.3")
 }
