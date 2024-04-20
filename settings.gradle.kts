55a3bbaa34db9e4edbbd51c9eb190e0546a0038d
deirn <deirn@bai.lol>
2024-04-20 22:58:27 +0700

    aaaaaaaaaaaaaaaaaa

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,7 @@ pluginManagement {
         gradlePluginPortal()
         maven("https://maven.fabricmc.net")
         maven("https://maven.neoforged.net/releases")
+        maven("https://maven.minecraftforge.net/")
         maven("https://maven.quiltmc.org/repository/release")
         maven("https://repo.spongepowered.org/repository/maven-public")
     }
