eec0fb3bc8e88bfcaece2dcd847ce484ff0340ed
deirn <deirn@bai.lol>
2024-06-17 22:49:31 +0700

    hide harvest tooltip on creative by default
    (cherry picked from commit 3bce6e1f31d339daccd939e1a444747e265aacd8)

--- src/pluginHarvest/java/mcp/mobius/waila/plugin/harvest/config/Options.java
+++ src/pluginHarvest/java/mcp/mobius/waila/plugin/harvest/config/Options.java
@@ -7,6 +7,7 @@ public class Options {
     // @formatter:off
     public static final ResourceLocation ENABLED      = rl("enabled");
     public static final ResourceLocation DISPLAY_MODE = rl("display_mode");
+    public static final ResourceLocation CREATIVE     = rl("creative");
     // @formatter:on
 
     public static ResourceLocation rl(String path) {
