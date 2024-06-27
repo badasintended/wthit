46495841d06c9cadb03bf9516a262d365b5a2508
deirn <deirn@bai.lol>
2024-06-27 19:53:43 +0700

    how come `canPerformAction` is not changed too? unless they want to make another breaking change just for the sake of it...

--- platform/neo/src/plugin/java/mcp/mobius/waila/plugin/neo/WailaPluginNeo.java
+++ platform/neo/src/plugin/java/mcp/mobius/waila/plugin/neo/WailaPluginNeo.java
@@ -15,7 +15,7 @@ import net.minecraft.world.level.block.DoublePlantBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.material.Fluid;
 import net.neoforged.neoforge.common.IShearable;
-import net.neoforged.neoforge.common.ToolActions;
+import net.neoforged.neoforge.common.ItemAbilities;
 
 public class WailaPluginNeo implements IWailaPlugin {
 
@@ -29,37 +29,37 @@ public class WailaPluginNeo implements IWailaPlugin {
         registrar.addToolType(ResourceLocation.withDefaultNamespace("pickaxe"), IToolType.builder()
             .lowestTierItem(Items.WOODEN_PICKAXE)
             .blockTag(BlockTags.MINEABLE_WITH_PICKAXE)
-            .itemPredicate(it -> it.canPerformAction(ToolActions.PICKAXE_DIG))
+            .itemPredicate(it -> it.canPerformAction(ItemAbilities.PICKAXE_DIG))
             .build());
 
         registrar.addToolType(ResourceLocation.withDefaultNamespace("shovel"), IToolType.builder()
             .lowestTierItem(Items.WOODEN_SHOVEL)
             .blockTag(BlockTags.MINEABLE_WITH_SHOVEL)
-            .itemPredicate(it -> it.canPerformAction(ToolActions.SHOVEL_DIG))
+            .itemPredicate(it -> it.canPerformAction(ItemAbilities.SHOVEL_DIG))
             .build());
 
         registrar.addToolType(ResourceLocation.withDefaultNamespace("axe"), IToolType.builder()
             .lowestTierItem(Items.WOODEN_AXE)
             .blockTag(BlockTags.MINEABLE_WITH_AXE)
-            .itemPredicate(it -> it.canPerformAction(ToolActions.AXE_DIG))
+            .itemPredicate(it -> it.canPerformAction(ItemAbilities.AXE_DIG))
             .build());
 
         registrar.addToolType(ResourceLocation.withDefaultNamespace("hoe"), IToolType.builder()
             .lowestTierItem(Items.WOODEN_HOE)
             .blockTag(BlockTags.MINEABLE_WITH_HOE)
-            .itemPredicate(it -> it.canPerformAction(ToolActions.HOE_DIG))
+            .itemPredicate(it -> it.canPerformAction(ItemAbilities.HOE_DIG))
             .build());
 
         registrar.addToolType(ResourceLocation.withDefaultNamespace("sword"), IToolType.builder()
             .lowestTierItem(Items.WOODEN_SWORD)
             .blockTag(BlockTags.SWORD_EFFICIENT)
-            .itemPredicate(it -> it.canPerformAction(ToolActions.SWORD_DIG))
+            .itemPredicate(it -> it.canPerformAction(ItemAbilities.SWORD_DIG))
             .build());
 
         registrar.addToolType(ResourceLocation.withDefaultNamespace("shears"), IToolType.builder()
             .lowestTierItem(Items.SHEARS)
             .blockPredicate(it -> it.getBlock() instanceof IShearable || it.getBlock() instanceof DoublePlantBlock)
-            .itemPredicate(it -> it.canPerformAction(ToolActions.SHEARS_DIG))
+            .itemPredicate(it -> it.canPerformAction(ItemAbilities.SHEARS_DIG))
             .build());
     }
 
