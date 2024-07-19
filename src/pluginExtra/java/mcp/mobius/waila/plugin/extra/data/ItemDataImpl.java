a2d05ca847b9039dc770a9e1691bc24871acc44d
deirn <deirn@bai.lol>
2024-07-19 23:29:56 +0700

    use registry codecs instead of manual read/writeById
    closes #275

--- src/pluginExtra/java/mcp/mobius/waila/plugin/extra/data/ItemDataImpl.java
+++ src/pluginExtra/java/mcp/mobius/waila/plugin/extra/data/ItemDataImpl.java
@@ -1,19 +1,22 @@
 package mcp.mobius.waila.plugin.extra.data;
 
 import java.util.ArrayList;
-import java.util.Objects;
 
 import mcp.mobius.waila.api.IData;
 import mcp.mobius.waila.api.IPluginConfig;
 import mcp.mobius.waila.api.data.ItemData;
+import net.minecraft.core.Holder;
 import net.minecraft.core.component.DataComponentPatch;
-import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
 public class ItemDataImpl extends ItemData {
 
+    private static final StreamCodec<RegistryFriendlyByteBuf, Holder<Item>> ITEM_CODEC = ByteBufCodecs.holderRegistry(Registries.ITEM);
     public static final StreamCodec<RegistryFriendlyByteBuf, ItemDataImpl> CODEC = StreamCodec.ofMember((d, buf) -> {
         var syncNbt = d.config.getBoolean(CONFIG_SYNC_NBT);
         buf.writeBoolean(syncNbt);
@@ -24,7 +27,7 @@ public class ItemDataImpl extends ItemData {
                 buf.writeBoolean(true);
             } else {
                 buf.writeBoolean(false);
-                buf.writeById(BuiltInRegistries.ITEM::getId, stack.getItem());
+                ITEM_CODEC.encode(buf, stack.getItemHolder());
                 buf.writeVarInt(stack.getCount());
                 if (syncNbt) DataComponentPatch.STREAM_CODEC.encode(buf, stack.getComponentsPatch());
             }
@@ -39,10 +42,11 @@ public class ItemDataImpl extends ItemData {
         for (var i = 0; i < size; i++) {
             if (buf.readBoolean()) continue;
 
-            var item = buf.readById(BuiltInRegistries.ITEM::byId);
+            var item = ITEM_CODEC.decode(buf);
             var count = buf.readVarInt();
-            var stack = new ItemStack(Objects.requireNonNull(item), count);
-            if (d.syncNbt) stack.applyComponents(DataComponentPatch.STREAM_CODEC.decode(buf));
+            var data = d.syncNbt ? DataComponentPatch.STREAM_CODEC.decode(buf) : DataComponentPatch.EMPTY;
+
+            var stack = new ItemStack(item, count, data);
             d.add(stack);
         }
 
