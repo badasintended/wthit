25deb73653b3056f8d8778d6842290bbae9a8016
deirn <deirn@bai.lol>
2024-07-20 02:15:32 +0700

    show ominous item spawner's item
    closes #274

--- src/pluginVanilla/java/mcp/mobius/waila/plugin/vanilla/provider/ItemHolderProvider.java
+++ src/pluginVanilla/java/mcp/mobius/waila/plugin/vanilla/provider/ItemHolderProvider.java
@@ -1,5 +1,7 @@
 package mcp.mobius.waila.plugin.vanilla.provider;
 
+import java.util.function.Function;
+
 import mcp.mobius.waila.api.IEntityAccessor;
 import mcp.mobius.waila.api.IEntityComponentProvider;
 import mcp.mobius.waila.api.IModInfo;
@@ -10,23 +12,32 @@ import mcp.mobius.waila.api.IWailaConfig;
 import mcp.mobius.waila.api.WailaConstants;
 import mcp.mobius.waila.api.component.ItemComponent;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.entity.OminousItemSpawner;
 import net.minecraft.world.entity.decoration.ItemFrame;
+import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
 
-public enum ItemFrameProvider implements IEntityComponentProvider {
+public enum ItemHolderProvider implements IEntityComponentProvider {
 
-    INSTANCE;
+    ITEM_FRAME(accessor -> accessor.<ItemFrame>getEntity().getItem()),
+    OMINOUS_ITEM_SPAWNER(accessor -> accessor.<OminousItemSpawner>getEntity().getItem());
+
+    final Function<IEntityAccessor, ItemStack> getter;
+
+    ItemHolderProvider(Function<IEntityAccessor, ItemStack> getter) {
+        this.getter = getter;
+    }
 
     @Nullable
     @Override
     public ITooltipComponent getIcon(IEntityAccessor accessor, IPluginConfig config) {
-        var stack = accessor.<ItemFrame>getEntity().getItem();
+        var stack = getter.apply(accessor);
         return stack.isEmpty() ? null : new ItemComponent(stack);
     }
 
     @Override
     public void appendHead(ITooltip tooltip, IEntityAccessor accessor, IPluginConfig config) {
-        var stack = accessor.<ItemFrame>getEntity().getItem();
+        var stack = getter.apply(accessor);
 
         if (!stack.isEmpty()) {
             var formatter = IWailaConfig.get().getFormatter();
@@ -40,13 +51,13 @@ public enum ItemFrameProvider implements IEntityComponentProvider {
 
     @Override
     public void appendBody(ITooltip tooltip, IEntityAccessor accessor, IPluginConfig config) {
-        var stack = accessor.<ItemFrame>getEntity().getItem();
+        var stack = getter.apply(accessor);
         ItemEntityProvider.appendBookProperties(tooltip, stack, config);
     }
 
     @Override
     public void appendTail(ITooltip tooltip, IEntityAccessor accessor, IPluginConfig config) {
-        var stack = accessor.<ItemFrame>getEntity().getItem();
+        var stack = getter.apply(accessor);
 
         if (config.getBoolean(WailaConstants.CONFIG_SHOW_MOD_NAME) && !stack.isEmpty()) {
             tooltip.setLine(WailaConstants.MOD_NAME_TAG, IWailaConfig.get().getFormatter().modName(IModInfo.get(stack).getName()));
