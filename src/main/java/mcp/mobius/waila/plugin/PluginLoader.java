31add160a5509386b9dd844beca64b2e1b9a0854
deirn <deirn@bai.lol>
2024-11-12 18:17:30 +0700

    delay first tick tasks until plugins finished loading
    closes #296
    
    (cherry picked from commit f426ef98231fdd502a8eca4bcbef6e8814fb60ae)

--- src/main/java/mcp/mobius/waila/plugin/PluginLoader.java
+++ src/main/java/mcp/mobius/waila/plugin/PluginLoader.java
@@ -54,6 +54,7 @@ public abstract class PluginLoader {
     );
 
     private boolean gathered = false;
+    public volatile boolean initialized = false;
 
     public static void reloadServerPlugins(MinecraftServer server) {
         PluginInfo.saveToggleConfig();
@@ -197,6 +198,7 @@ public abstract class PluginLoader {
         PluginConfig.reload();
 
         JsonConfig.reloadAllInstances();
+        initialized = true;
     }
 
     private void initialize(PluginInfo info) {
