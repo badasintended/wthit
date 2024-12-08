c11edb0fd030dd45295a34f05214ef4c189f7cbf
deirn <deirn@bai.lol>
2024-12-08 22:37:11 +0700

    update changelog

--- platform/neo/src/main/java/mcp/mobius/waila/neo/NeoApiService.java
+++ platform/neo/src/main/java/mcp/mobius/waila/neo/NeoApiService.java
@@ -2,7 +2,6 @@ package mcp.mobius.waila.neo;
 
 import mcp.mobius.waila.api.IModInfo;
 import mcp.mobius.waila.service.ApiService;
-import mcp.mobius.waila.util.ModInfo;
 import net.minecraft.world.item.ItemStack;
 
 public class NeoApiService extends ApiService {
