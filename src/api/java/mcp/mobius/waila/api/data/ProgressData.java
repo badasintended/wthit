17ac13f3ccd4fb9cd01d4e82ccc5a051542e2ea9
Dimas Firmansyah <deirn@bai.lol>
2025-10-09 06:40:54 +0700

    show progress as a bar only by default

--- src/api/java/mcp/mobius/waila/api/data/ProgressData.java
+++ src/api/java/mcp/mobius/waila/api/data/ProgressData.java
@@ -24,6 +24,8 @@ public abstract class ProgressData implements IData {
     public static final Type<ProgressData> TYPE = IData.createType(ID);
 
     public static final ResourceLocation CONFIG_TIME = BuiltinDataUtil.rl("progress.time");
+    public static final ResourceLocation CONFIG_BAR_HEIGHT = BuiltinDataUtil.rl("progress.height");
+    public static final ResourceLocation CONFIG_REPLACE_ITEMS = BuiltinDataUtil.rl("progress.replace_items");
 
     /**
      * Creates a progress data.
