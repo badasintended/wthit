b2cf8f489ef16acd58163d431f768fe495326d64
Dimas Firmansyah <deirn@bai.lol>
2025-10-15 22:37:20 +0700

    rename rootProject

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-rootProject.name = "wthit"
+rootProject.name = "wthit-master"
 
 fun platform(name: String) {
     include(name)
