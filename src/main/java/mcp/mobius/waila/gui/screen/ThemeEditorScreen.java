38748707e4360cd84b9108bbc855d3f21ff6e081
Dimas Firmansyah <deirn@bai.lol>
2025-10-18 09:04:53 +0700

    fix theme editor reset value when switching type
    (cherry picked from commit b5e4a36d27aadf69495a5a8696f1d6ad8c92d017)

--- src/main/java/mcp/mobius/waila/gui/screen/ThemeEditorScreen.java
+++ src/main/java/mcp/mobius/waila/gui/screen/ThemeEditorScreen.java
@@ -100,6 +100,7 @@ class ThemeEditorScreen extends ConfigScreen {
                         type = Registrar.get().themeTypes.get(ResourceLocation.parse(value));
                         themeAttrCategory.clear(options);
                         options.children().remove(themeAttrCategory);
+                        options.values.removeIf(it -> it.category == themeAttrCategory);
                         addTypeProperties(options);
                         options.init();
                         options.setFocused(this);
