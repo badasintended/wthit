a3fb46ebb63d480b9735fc031d9204a7c0ccaa24
Dimas Firmansyah <deirn@bai.lol>
2025-10-24 18:08:08 +0700

    fade out breaking progress instead of moving backwards when stopped
    closes #300
    
    (cherry picked from commit 1de2fa99f7a25d952472749c29a78f4615eda892)

--- src/pluginVanilla/java/mcp/mobius/waila/plugin/vanilla/provider/BreakProgressProvider.java
+++ src/pluginVanilla/java/mcp/mobius/waila/plugin/vanilla/provider/BreakProgressProvider.java
@@ -7,7 +7,7 @@ import mcp.mobius.waila.api.ICommonAccessor;
 import mcp.mobius.waila.api.IEventListener;
 import mcp.mobius.waila.api.IPluginConfig;
 import mcp.mobius.waila.api.util.WRenders;
-import mcp.mobius.waila.mixin.MultiPlayerGameModeAccess;
+import mcp.mobius.waila.mixed.MMultiPlayerGameMode;
 import mcp.mobius.waila.plugin.vanilla.config.Options;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
@@ -19,42 +19,60 @@ public enum BreakProgressProvider implements IEventListener {
 
     INSTANCE;
 
+    static final int FADE_TICK = 3;
+
     boolean wasBreaking = false;
-    float progressDelayTimer = 0;
+    float targetProgress = 0;
     float lastProgress = 0;
-    float lastTargetProgress = 0;
+
+    boolean fadeFinish = false;
+    int fadeTicks = 0;
+    float fade = 0;
+
+    @Override
+    public void onTick(IPluginConfig config) {
+        targetProgress = 0;
+        if (fadeFinish) {
+            fade = 0;
+            lastProgress = 0f;
+            fadeFinish = false;
+        }
+
+        if (!config.getBoolean(Options.BREAKING_PROGRESS)) return;
+        var gameMode = (MMultiPlayerGameMode) Objects.requireNonNull(Minecraft.getInstance().gameMode);
+        targetProgress = gameMode.wthit_destroyProgress();
+
+        var isBreaking = targetProgress > 0;
+        if (wasBreaking && !isBreaking) {
+            fadeTicks = FADE_TICK;
+            fade = fadeTicks;
+            if (gameMode.wthit_wasDestroyed()) targetProgress = 1;
+        }
+
+        wasBreaking = isBreaking;
+        if (fadeTicks > 0) {
+            fadeTicks--;
+            if (fadeTicks == 0) fadeFinish = true;
+        }
+    }
 
     @Override
     public void onAfterTooltipRender(GuiGraphics ctx, Rectangle rect, ICommonAccessor accessor, IPluginConfig config) {
-        if (!config.getBoolean(Options.BREAKING_PROGRESS)) return;
-
-        var gameMode = Objects.requireNonNull(Minecraft.getInstance().gameMode);
-        var gameModeAccess = (MultiPlayerGameModeAccess) gameMode;
-
         var dt = Minecraft.getInstance().getDeltaTracker().getRealtimeDeltaTicks();
 
-        var isBreaking = gameMode.isDestroying();
-        var targetProgress = gameModeAccess.wthit_destroyProgress();
-        if (!isBreaking && wasBreaking && progressDelayTimer <= 0f) {
-            progressDelayTimer = 4f;
+        var progress = lastProgress;
+        if (targetProgress > lastProgress) {
+            var progressChange = (targetProgress - lastProgress) * dt;
+            progress = Mth.clamp(lastProgress + progressChange, 0f, 1f);
         }
 
-        var isInDelay = progressDelayTimer > 0;
-        if (isInDelay) {
-            targetProgress = gameModeAccess.wthit_destroyDelay() > 0 ? 1f : lastTargetProgress;
-        }
+        lastProgress = progress;
+        if (progress <= 0f && fade <= 0) return;
 
-        var progressDiff = targetProgress - lastProgress;
-        var progressChangeAmount = progressDiff * dt;
-        var actualProgress = Mth.clamp(lastProgress + progressChangeAmount, 0f, 1f);
-
-        var lineLength = 0f;
-
-        if (config.getBoolean(Options.BREAKING_PROGRESS_BOTTOM_ONLY)) {
-            lineLength = (rect.width - 2) * actualProgress;
-        } else {
-            lineLength = ((rect.width + rect.height - 4) * 2) * actualProgress;
-        }
+        var bottomOnly = config.getBoolean(Options.BREAKING_PROGRESS_BOTTOM_ONLY);
+        var lineLength = bottomOnly
+            ? (rect.width - 2) * progress
+            : (((rect.width - 2) + (rect.height - 2)) * 2) * progress;
 
         if (lineLength > 0) {
             var bufferSource = WRenders.bufferSource(ctx);
@@ -65,35 +83,36 @@ public enum BreakProgressProvider implements IEventListener {
             var y = rect.y + rect.height - 2;
 
             var color = config.getInt(Options.BREAKING_PROGRESS_COLOR);
-            fill(ctx, bufferSource, x, y, x + Math.min(lineLength, hLength), y + 1, color);
+            var alpha = fade > 0 ? fade / FADE_TICK : 1.0f;
+            var a = (int) (alpha * ((color >> 24) & 0xFF));
+            color = (color & 0x00FFFFFF) | (a << 24);
+
+            fill(ctx, bufferSource, x, y, x + Math.round(lineLength, hLength), y + 1, color);
             lineLength -= hLength;
 
             if (lineLength > 0) {
                 x = rect.x + rect.width - 2;
                 y = rect.y + rect.height - 2;
-                fill(ctx, bufferSource, x, y, x + 1, y - Math.min(lineLength, vLength), color);
+                fill(ctx, bufferSource, x, y, x + 1, y - Math.round(lineLength, vLength), color);
                 lineLength -= vLength;
 
                 if (lineLength > 0) {
                     x = rect.x + rect.width - 1;
                     y = rect.y + 1;
-                    fill(ctx, bufferSource, x, y, x - Math.min(lineLength, hLength), y + 1, color);
+                    fill(ctx, bufferSource, x, y, x - Math.round(lineLength, hLength), y + 1, color);
                     lineLength -= hLength;
 
                     if (lineLength > 0) {
                         x = rect.x + 1;
                         y = rect.y + 2;
-                        fill(ctx, bufferSource, x, y, x + 1, y + Math.min(lineLength, vLength), color);
+                        fill(ctx, bufferSource, x, y, x + 1, y + Math.round(lineLength, vLength), color);
                     }
                 }
             }
             ctx.flush();
         }
 
-        wasBreaking = isBreaking;
-        lastProgress = actualProgress;
-        lastTargetProgress = targetProgress;
-        if (isInDelay) progressDelayTimer -= dt;
+        if (fade > 0) fade = Math.max(fade - dt, 0f);
     }
 
     private void fill(GuiGraphics ctx, MultiBufferSource bufferSource, float x1, float y1, float x2, float y2, int color) {
