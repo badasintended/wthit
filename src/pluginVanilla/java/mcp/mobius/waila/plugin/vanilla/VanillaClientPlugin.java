2039863fe839d2cfb6cac3d9acbec66280d44f9a
Dimas Firmansyah <deirn@bai.lol>
2026-01-02 03:30:05 +0700

    add lectern book tooltip
    closes #352
    
    (cherry picked from commit b6254a209e6a1c285166d64c026f6453917fd1f2)

--- src/pluginVanilla/java/mcp/mobius/waila/plugin/vanilla/VanillaClientPlugin.java
+++ src/pluginVanilla/java/mcp/mobius/waila/plugin/vanilla/VanillaClientPlugin.java
@@ -21,6 +21,7 @@ import mcp.mobius.waila.plugin.vanilla.provider.InvisibleEntityProvider;
 import mcp.mobius.waila.plugin.vanilla.provider.ItemEntityProvider;
 import mcp.mobius.waila.plugin.vanilla.provider.ItemHolderProvider;
 import mcp.mobius.waila.plugin.vanilla.provider.JukeboxProvider;
+import mcp.mobius.waila.plugin.vanilla.provider.LecternProvider;
 import mcp.mobius.waila.plugin.vanilla.provider.MobEffectProvider;
 import mcp.mobius.waila.plugin.vanilla.provider.MobTimerProvider;
 import mcp.mobius.waila.plugin.vanilla.provider.NoteBlockProvider;
@@ -69,6 +70,7 @@ import net.minecraft.world.level.block.TrappedChestBlock;
 import net.minecraft.world.level.block.entity.BannerBlockEntity;
 import net.minecraft.world.level.block.entity.BeaconBlockEntity;
 import net.minecraft.world.level.block.entity.JukeboxBlockEntity;
+import net.minecraft.world.level.block.entity.LecternBlockEntity;
 import net.minecraft.world.level.block.entity.SkullBlockEntity;
 import net.minecraft.world.level.block.entity.SpawnerBlockEntity;
 import net.minecraft.world.level.material.Fluids;
@@ -163,6 +165,11 @@ public class VanillaClientPlugin implements IWailaClientPlugin {
         registrar.head(SelectableSlotContainerProvider.BOOKSHELF, ChiseledBookShelfBlock.class);
         registrar.body(SelectableSlotContainerProvider.BOOKSHELF, ChiseledBookShelfBlock.class);
         registrar.tail(SelectableSlotContainerProvider.BOOKSHELF, ChiseledBookShelfBlock.class);
+
+        registrar.icon(LecternProvider.INSTANCE, LecternBlockEntity.class);
+        registrar.head(LecternProvider.INSTANCE, LecternBlockEntity.class);
+        registrar.body(LecternProvider.INSTANCE, LecternBlockEntity.class);
+        registrar.tail(LecternProvider.INSTANCE, LecternBlockEntity.class);
 
         FluidData.describeFluid(Fluids.WATER, WaterDescriptor.INSTANCE);
         FluidData.describeFluid(Fluids.LAVA, LavaDescriptor.INSTANCE);
--- src/pluginVanilla/java/mcp/mobius/waila/plugin/vanilla/config/Options.java
+++ src/pluginVanilla/java/mcp/mobius/waila/plugin/vanilla/config/Options.java
@@ -55,6 +55,7 @@ public final class Options {
     public static final ResourceLocation EFFECT_HIDDEN_MOB             = rl("effect.hidden_mob");
     public static final ResourceLocation SHELF_ITEMS                   = rl("shelf.items");
     public static final ResourceLocation BOOK_BOOKSHELF                = rl("book.bookshelf");
+    public static final ResourceLocation BOOK_LECTERN                  = rl("book.lectern");
     public static final ResourceLocation BOOK_WRITTEN                  = rl("book.written");
     public static final ResourceLocation BOOK_ENCHANTMENT_DISPLAY_MODE = rl("book.enchantment");
     public static final ResourceLocation BOOK_ENCHANTMENT_CYCLE_TIMING = rl("book.enchantment_cycle_timing");
